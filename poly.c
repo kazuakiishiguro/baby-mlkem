d2f432c43bba054a035da9990f120b31a86a74e6
Kazuaki Ishiguro <kazuakiishiguro@protonmail.com>
2025-08-26 12:43:18 +0900

    simplify poly add/sub

--- poly.c
+++ poly.c
@@ -7,9 +7,7 @@
 void poly256_add(const poly256 a, const poly256 b, poly256 out) {
   for (int i = 0; i < N; i++) {
     int32_t tmp = (int32_t)a[i] + (int32_t)b[i];
-    tmp %= Q;
-    if (tmp < 0) tmp += Q;
-    out[i] = (int16_t)tmp;
+    out[i] = (tmp >= Q) ? (tmp - Q) : tmp;
   }
 }
 
@@ -20,8 +18,9 @@ void poly256_add(const poly256 a, const poly256 b, poly256 out) {
 void poly256_sub(const poly256 a, const poly256 b, poly256 out) {
   for (int i = 0; i < N; i++) {
     int32_t tmp = (int32_t)a[i] - (int32_t)b[i];
-    tmp %= Q;
-    if (tmp < 0) tmp += Q;
-    out[i] = (int16_t)tmp;
+    if (tmp < 0) {
+      tmp += Q;
+    }
+    out[i] = (tmp >= Q) ? (tmp - Q) : tmp;
   }
 }
