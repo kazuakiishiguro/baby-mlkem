56e1992b98aa8eee9d3f87fd5684459929f0a71a
Kazuaki Ishiguro <kazuakiishiguro@protonmail.com>
2025-08-26 13:18:36 +0900

    introduce reduce files

---
+++ reduce.c
@@ -0,0 +1,15 @@
+#include "reduce.h"
+#include "params.h"
+
+static const int16_t q_inv = -3327 // Q^(-1) mod 2^16
+
+int16_t montgomery_reduce(int16_t a) {
+  int32_t t;
+  int16_t u;
+
+  u = (int16_t)(a * q_inv);
+  t = (int32_t)u * Q;
+  t = a - t;
+  t >>= 16;
+  return (int16_t)t;
+}
---
+++ reduce.h
@@ -0,0 +1,8 @@
+#ifndef REDUCE_H
+#define REDUCE_H
+
+#include <stdint.h>
+
+int16_t montgomery_reduce(int16_t a);
+
+#endif /* REDUCE_H */
