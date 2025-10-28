60ced086b13ab1379054724dc16450237ea72d0a
Kazuaki Ishiguro <kazuakiishiguro@protonmail.com>
2025-10-28 16:04:56 +0900

    typo

--- reduce.c
+++ reduce.c
@@ -1,7 +1,7 @@
 #include "reduce.h"
 #include "params.h"
 
-static const int16_t q_inv = -3327 // Q^(-1) mod 2^16
+static const int16_t q_inv = -3327; // Q^(-1) mod 2^16
 
 int16_t montgomery_reduce(int16_t a) {
   int32_t t;
