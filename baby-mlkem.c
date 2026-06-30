b7ccfd953ae7cc270e5bf1647b9e48b696884c9f
Kazuaki Ishiguro <kazuakiishiguro@protonmail.com>
2026-06-30 12:43:07 +0900

    perf: vectorize inverse ntt tail with avx512

--- baby-mlkem.c
+++ baby-mlkem.c
@@ -751,6 +751,34 @@ static void ntt_head_avx512(poly256 f) {
     }
   }
 }
+
+static inline void ntt_inv_butterfly16_avx512(int16_t *a_ptr,
+                                              int16_t *b_ptr,
+                                              __m512i zeta) {
+  __m256i a16 = _mm256_loadu_si256((const __m256i *)a_ptr);
+  __m256i b16 = _mm256_loadu_si256((const __m256i *)b_ptr);
+  __m512i a = _mm512_cvtepu16_epi32(a16);
+  __m512i b = _mm512_cvtepu16_epi32(b16);
+  __m512i diff = mod_q_sub_i32x16(b, a);
+  __m512i t = mod_q_reduce_ntt_u32x16(_mm512_mullo_epi32(diff, zeta));
+  _mm256_storeu_si256((__m256i *)a_ptr,
+                      _mm512_cvtusepi32_epi16(mod_q_add_i32x16(a, b)));
+  _mm256_storeu_si256((__m256i *)b_ptr, _mm512_cvtusepi32_epi16(t));
+}
+
+static void ntt_inv_tail_avx512(poly256 f) {
+  int k = 15;
+  for (int log2len = 4; log2len <= 7; log2len++) {
+    int length = (1 << log2len);
+    for (int start = 0; start < N; start += (2 * length)) {
+      __m512i zeta = _mm512_set1_epi32(ZETA[k--]);
+      for (int j = 0; j < length; j += 16) {
+        ntt_inv_butterfly16_avx512(f + start + j, f + start + j + length,
+                                   zeta);
+      }
+    }
+  }
+}
 #endif
 
 static inline void ntt_butterfly8_avx2(int16_t *a_ptr, int16_t *b_ptr,
@@ -1052,14 +1080,13 @@ static void ntt(const poly256 f_in, poly256 f_out) {
 
 /* NTT^-1 */
 static inline void ntt_inv_butterflies_inplace(poly256 out) {
-  int k = 127;
 #if defined(__AVX2__)
   ntt_inv_head_avx2(out);
-  k = 15;
-  for (int log2len = 4; log2len <= 7; log2len++) {
+#if defined(__AVX512F__) && defined(__AVX512BW__)
+  ntt_inv_tail_avx512(out);
 #else
-  for (int log2len = 1; log2len <= 7; log2len++) {
-#endif
+  int k = 15;
+  for (int log2len = 4; log2len <= 7; log2len++) {
     int length = (1 << log2len);
     for (int start = 0; start < N; start += (2 * length)) {
       uint16_t zeta = ZETA[k--];
@@ -1077,6 +1104,28 @@ static inline void ntt_inv_butterflies_inplace(poly256 out) {
       }
     }
   }
+#endif
+#else
+  int k = 127;
+  for (int log2len = 1; log2len <= 7; log2len++) {
+    int length = (1 << log2len);
+    for (int start = 0; start < N; start += (2 * length)) {
+      uint16_t zeta = ZETA[k--];
+#if defined(__clang__)
+#pragma clang loop vectorize_width(16) interleave_count(1)
+#endif
+      for (int j = 0; j < length; j++) {
+        int idx = start + j;
+        int16_t t = out[idx];
+        int16_t u = out[idx + length];
+        out[idx] = mod_q_add_i16(t, u);
+        int16_t tmp2 = mod_q_sub_i16(u, t);
+        uint32_t tmp3 = (uint32_t)(uint16_t)tmp2 * (uint32_t)zeta;
+        out[idx + length] = mod_q_reduce_ntt_u32(tmp3);
+      }
+    }
+  }
+#endif
 }
 
 static inline void ntt_inv_scale(poly256 out) {
